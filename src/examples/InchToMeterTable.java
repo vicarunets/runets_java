5bcb60c82f768625c84a4e36e56a6a7ceb0cac27
Рунец <travel.vi@mail.ru>
2017-11-28 11:24:37 +0300

    Homework (28/11/2017) End of Lesson 1

---
+++ src/examples/InchToMeterTable.java
@@ -0,0 +1,17 @@
+package examples;
+
+public class InchToMeterTable {
+    public static void main(String[] args) {
+    double inch,meter;
+    int counter=0;
+        for (inch=1;inch<=144;inch++) {
+            meter = inch / 39.37;
+            System.out.println(inch + " дюймам соответствует " + meter + " метра");
+            counter++;
+            if (counter == 12) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
---
+++ src/examples/MoonWeight.java
@@ -0,0 +1,10 @@
+package examples;
+
+public class MoonWeight {
+    public static void main(String[] args) {
+    double earthweight; double moonweight;
+    earthweight=60;
+    moonweight=earthweight*0.17;
+        System.out.println("Мой вес на Луне = "+moonweight);
+    }
+}
